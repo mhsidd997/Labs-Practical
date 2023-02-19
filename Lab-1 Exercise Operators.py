4d8ffaf5f11d7887849a2b91fd565930ad6cad36
Hamza Siddiqui <hamza.siddiqui@encore-pay.com>
2023-02-19 14:19:20 +0500

    File Updated

--- Lab-1 Exercise Operators.py
+++ Lab-1 Exercise Operators.py
@@ -16,3 +16,13 @@ if (type(y) is not int):
     print("true")
 else:
     print("false")
+
+# Membership Operator in Python
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9]
+
+for item in list1:
+    if item in list2:
+        print("overlapping")
+    else:
+        print("not overlapping")

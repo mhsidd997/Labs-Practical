89c02e7ec21e86d52641599bde1b1d2a208b42ba
Hamza Siddiqui <hamza.siddiqui@encore-pay.com>
2023-02-19 14:12:55 +0500

    Lab-1 Exercise Operators Updated Lab-1 Exercise Membership Operator

--- Lab-1 Exercise Operators.py
+++ Lab-1 Exercise Operators.py
@@ -4,3 +4,15 @@ if (type(x) is int):
     print("true")
 else:
     print("false")
+
+# if(type(y) y = 7.2    Written same as on the Lab File
+# if (type(y) is not int):
+#     print("true")
+# else:
+#     print("false")
+
+y = 7.2
+if (type(y) is not int):
+    print("true")
+else:
+    print("false")
